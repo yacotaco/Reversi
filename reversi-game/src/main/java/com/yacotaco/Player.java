ae4ed4ceebbf5151ec86c73b96e48711140eb7c8
yacotaco <kamilkurach@yahoo.pl>
2019-06-24 09:18:12 +0200

    Add player model

--- reversi-game/src/main/java/com/yacotaco/Player.java
+++ reversi-game/src/main/java/com/yacotaco/Player.java
@@ -4,5 +4,31 @@ package com.yacotaco;
  * Player
  */
 public class Player {
-        
+    private String name;
+    private Integer points;
+
+    /**
+     * @param playerName player name
+     * @param points     scored points
+     */
+
+    public Player() {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
 }
