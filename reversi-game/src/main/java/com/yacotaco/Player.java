73ef5c6c6be55fca40a2b602a4cbd6d9a8524a2c
yacotaco <kamilkurach@protonmail.com>
2019-10-28 19:31:25 +0100

    Add disc state to player. Format code

--- reversi-game/src/main/java/com/yacotaco/Player.java
+++ reversi-game/src/main/java/com/yacotaco/Player.java
@@ -6,10 +6,13 @@ package com.yacotaco;
 public class Player {
     private String name;
     private Integer points;
+    private Integer discState;
 
     /**
      * @param playerName player name
      * @param points     scored points
+     * @param discState  stores inforamtion which disc color(state) was assigned to
+     *                   player
      */
 
     public Player() {
@@ -30,5 +33,13 @@ public class Player {
 
     public void setPoints(Integer points) {
         this.points = points;
+    }
+
+    public Integer getDiscState() {
+        return discState;
+    }
+
+    public void setDiscState(Integer discState) {
+        this.discState = discState;
     }
 }
