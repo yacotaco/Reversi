9ff584660584e859e6095b28140de230e510db3c
yacotaco <kamilkurach@protonmail.com>
2020-11-30 20:16:24 +0100

    Add: AI Player flag

--- reversi-game/src/main/java/com/yacotaco/Controller.java
+++ reversi-game/src/main/java/com/yacotaco/Controller.java
@@ -47,6 +47,7 @@ public class Controller {
     private final Integer initPlayerTurn = 0;
     private final Boolean DEBUG_MARKER = true;
     private final Boolean MOVE_MARKER = true;
+    private final Boolean AI_PLAYER = false;
     private ArrayList<Integer[]> allValidMoves = new ArrayList<Integer[]>();
     private ArrayList<Disc> flipedDiscsToMark = new ArrayList<Disc>();
 
