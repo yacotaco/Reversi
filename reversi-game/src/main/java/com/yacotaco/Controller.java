8b4062adf6aa15452fc08d2f18b47b28e1f6419e
yacotaco <kamilkurach@protonmail.com>
2020-12-03 13:21:16 +0100

    Remove: unused imports

--- reversi-game/src/main/java/com/yacotaco/Controller.java
+++ reversi-game/src/main/java/com/yacotaco/Controller.java
@@ -10,8 +10,6 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.Optional;
-import java.util.Timer;
-import java.util.TimerTask;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
