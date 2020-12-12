f935dedda96179741026f83163c908f1dbef377a
yacotaco <kamilkurach@protonmail.com>
2020-12-12 06:38:25 +0100

    Add: javadoc comments

--- reversi-game/src/main/java/com/yacotaco/Disc.java
+++ reversi-game/src/main/java/com/yacotaco/Disc.java
@@ -1,36 +1,74 @@
 package com.yacotaco;
 
 /**
- * Disc
+ * Disc class.
+ *
+ * @author Kamil Kurach
+ * @author https://github.com/yacotaco
+ * @version 1.0
  */
 public class Disc {
+    /** Position in column. */
     private Integer col;
+    /** Position in row. */
     private Integer row;
+    /** Disc state. */
     private Integer state;
 
+    /** Disc constructor. */
     public Disc() {
     }
 
+    /**
+     * Gets disc column coordinate.
+     *
+     * @return column value.
+     */
     public Integer getCol() {
         return col;
     }
 
-    public void setCol(Integer col) {
+    /**
+     * Sets disc column coordinate.
+     *
+     * @param col position in column.
+     */
+    public void setCol(final Integer col) {
         this.col = col;
     }
 
+    /**
+     * Gets disc row coordinate.
+     *
+     * @return position in column.
+     */
     public Integer getRow() {
         return row;
     }
 
+    /**
+     * Sets disc row coordiante.
+     *
+     * @param row position in row.
+     */
     public void setRow(Integer row) {
         this.row = row;
     }
 
+    /**
+     * Gets disc state.
+     *
+     * @return disc state.
+     */
     public Integer getState() {
         return state;
     }
 
+    /**
+     * Sets disc state.
+     *
+     * @param state disc state.
+     */
     public void setState(Integer state) {
         this.state = state;
     };
