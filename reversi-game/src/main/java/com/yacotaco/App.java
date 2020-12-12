7cef0014fa5033a3e6d00044665a6b18295bc9e2
yacotaco <kamilkurach@protonmail.com>
2020-12-12 06:52:52 +0100

    Add: javadoc and format code style

--- reversi-game/src/main/java/com/yacotaco/App.java
+++ reversi-game/src/main/java/com/yacotaco/App.java
@@ -4,18 +4,27 @@ import javafx.application.Application;
 import javafx.stage.Stage;
 
 /**
- * JavaFX App
+ * App class. Inits and binds all modules.
+ *
+ * @author Kamil Kurach
+ * @author https://github.com/yacotaco
+ * @version 1.0
  */
-public class App extends Application {
+public final class App extends Application {
 
     @Override
-    public void start(Stage stage) {
+    public void start(final Stage stage) {
         Board board = new Board();
         View view = new View(stage);
         Controller controller = new Controller(board, view, stage);
     }
 
-    public static void main(String[] args) {
+    /**
+     * App main function.
+     *
+     * @param args args.
+     */
+    public static void main(final String[] args) {
         launch();
     }
 
