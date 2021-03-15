6746740f26c2fbad9de787fb815385050f428291
yacotaco <kamilkurach@protonmail.com>
2021-03-15 13:43:37 +0100

    Add: javadoc comments

--- reversi-game/src/main/java/com/yacotaco/ai_modules/MCTS.java
+++ reversi-game/src/main/java/com/yacotaco/ai_modules/MCTS.java
@@ -5,11 +5,16 @@ package com.yacotaco.ai_modules;
 
 /** MCTS AI module. */
 public class MCTS {
+    /** Selection class object. */
     private Selection selection;
+    /** Expansion class object. */
     private Expansion expansion;
+    /** Simulation class object. */
     private Simulation simulation;
+    /** Backpropagation class object. */
     private Backpropagation backpropagation;
 
+    /** MCTS class constructor. */
     public MCTS() {
         this.selection = new Selection();
         this.expansion = new Expansion();
